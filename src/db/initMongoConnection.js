283e4838c0241b53ee3ac13ce63b49e3bec34547
Olha <oyashnaya88@gmail.com>
2025-02-23 23:56:47 +0200

    main info

---
+++ src/db/initMongoConnection.js
@@ -0,0 +1,23 @@
+import mongoose from 'mongoose';
+import { env } from '../utils/env.js';
+
+
+export const initMongoConnection = async () => {
+  try {
+    const user = env('MONGODB_USER');
+    const pwd = env('MONGODB_PASSWORD');
+    const url = env('MONGODB_URL');
+    const db = env('MONGODB_DB');
+
+    await mongoose.connect(
+      `mongodb+srv://${user}:${pwd}@${url}/${db}?retryWrites=true&w=majority&appName=Cluster01`,
+    );
+
+    // mongodb+srv://oyashnaya88:<db_password>@cluster0.xhjrb.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0
+
+    console.log(`Mongo connection successfully established!`);
+  } catch (error) {
+    console.error(`Error while setting up mongo connection`, error);
+    throw error;
+  }
+};
