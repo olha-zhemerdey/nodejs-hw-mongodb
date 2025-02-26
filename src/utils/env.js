628746abbe0229d8a5e397bd65daba0b9643c8f0
Olha <oyashnaya88@gmail.com>
2025-02-26 20:37:08 +0200

    last changes

--- src/utils/env.js
+++ src/utils/env.js
@@ -1,8 +1,13 @@
-import 'dotenv/config';
+import dotenv from 'dotenv';
 
-export function env(name, defaultValue) {
+dotenv.config();
+
+export const env = (name, defaultValue) => {
   const value = process.env[name];
+
   if (value) return value;
+
   if (defaultValue) return defaultValue;
-  throw new Error(`Missing process.env[${name}]`);
-}
+
+  throw new Error(`Missing: process.env['${name}'].`);
+};
